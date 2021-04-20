ef521fd00ba09a2f74af564b63628b2dc90cdefc
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-04-20 14:51:24 -0500

    added files from previous project

--- src/App/index.js
+++ src/App/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
 
   const handleClick = (e) => {
     console.warn(`You clicked ${e.target.id}`);
@@ -11,26 +10,7 @@ function App() {
 
   return (
     <div className='App'>
-      <h2>INSIDE APP COMPONENT</h2>
-      <div>
-        <button
-          id='this-button'
-          className='btn btn-info'
-          onClick={handleClick}
-        >
-          I am THIS button
-        </button>
-      </div>
-      <div>
-        <button
-          id='that-button'
-          className='btn btn-primary mt-3'
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
+     
     </div>
   );
 }
