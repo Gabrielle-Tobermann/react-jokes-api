4e569e7512b4835843b7b67492b9b80ab6b78f7a
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-04-20 15:22:08 -0500

    button prints to console

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
+import { Button } from 'reactstrap';
+// import getJoke from '../App/helpers/data/jokeData';
 
 function App() {
+  // const [allJokes, setAllJokes] = useState([]);
+  // const [singleJoke, setSingleJoke] = useState({});
 
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
+  const handleClick = () => {
+    console.warn('You clicked the button woo');
   };
 
   return (
     <div className='App'>
-     
+     <Button color="info" onClick={handleClick}>Click</Button>
     </div>
   );
 }
