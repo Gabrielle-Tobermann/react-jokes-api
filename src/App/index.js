0d6ead2387e5a0121e14aef9383826ff91f35af8
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-04-20 20:11:57 -0500

    refactored code

--- src/App/index.js
+++ src/App/index.js
@@ -6,17 +6,36 @@ import getJoke from './helpers/data/jokeData';
 function App() {
   const [jokes, setJokes] = useState([]);
   const [singleJoke, setSingleJoke] = useState({});
+  const [showJoke, setShowJoke] = useState(false);
+  const [showPunchline, setShowPunchline] = useState(false);
+  const [showNewJoke, setShowNewJoke] = useState(false);
 
   const handleClick = () => {
-    console.warn(jokes);
-    setSingleJoke(jokes[0]);
-    console.warn(singleJoke.setup);
+    if (showJoke) {
+      setShowJoke(false);
+      if (showPunchline) {
+        setShowPunchline(false);
+      } else {
+        setShowPunchline(true);
+      }
+      // setShowPunchline(false);
+    } else {
+      setShowJoke(true);
+      setSingleJoke(jokes[Math.floor(Math.random() * jokes.length)]);
+      // setShowPunchline(false);
+    }
+    if (showNewJoke) {
+      setShowNewJoke(false);
+    } else {
+      showNewJoke(true);
+      setSingleJoke(jokes[Math.floor(Math.random() * jokes.length)]);
+    }
   };
 
   useEffect(() => {
     getJoke().then((items) => {
       setJokes(items);
-      setSingleJoke(items[0]);
+      setSingleJoke(items[Math.floor(Math.random() * jokes.length)]);
     });
   }, []);
 
@@ -24,12 +43,16 @@ function App() {
     <div className='App'>
      <div className="card">
     <div className="card-body d-flex flex-column">
-      {/* </div><img src="{jokeGenerator}" class="card-img-top" alt="..."> */}
       <h6 className="card-subtitle mb-2 text-muted"></h6>
-      <p className="card-text">{singleJoke.setup}</p>
-      <p className="card-text">This will be punchline</p>
+      <p className="card-text">{showJoke || showPunchline ? singleJoke.setup : ''}</p>
+      <p className="card-text">{showPunchline ? singleJoke.punchline : ''}</p>
       <div className="mt-auto">
-        <Button color="info" onClick={handleClick}>Click</Button>
+        <div> {showJoke ? '' : <Button color="info" onClick={handleClick}>Get a Joke</Button>}
+        </div>
+        <div> {showPunchline ? '' : <Button color="info" onClick={handleClick}>Get Punchline</Button>}
+        </div>
+        <div> {showNewJoke ? '' : <Button color="info" onClick={handleClick}>Get a New Joke</Button>}
+        </div>
       </div>
   </div>
 </div>
